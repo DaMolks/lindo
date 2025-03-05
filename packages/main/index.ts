c23755bf4dbef01bda1f591c28461e0a7af4508b
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-03-05 21:39:21 +0100

    Ajout de la gestion du protocole dofustouch:// dans le fichier principal

--- packages/main/index.ts
+++ packages/main/index.ts
@@ -22,10 +22,55 @@ electronLocalshortcut.setKeyboardLayout(getCurrentKeyboardLayout(), getKeyMap())
 // Set application name for Windows 10+ notifications
 if (process.platform === 'win32') app.setAppUserModelId(app.getName())
 
+// Register as handler for dofustouch:// protocol
+if (process.defaultApp) {
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient('dofustouch', process.execPath, [process.argv[1]])
+  }
+} else {
+  app.setAsDefaultProtocolClient('dofustouch')
+}
+
 if (!app.requestSingleInstanceLock()) {
   app.quit()
   process.exit(0)
 }
+
+// Handle the protocol. In this case, we choose to show an existing game window or create a new one.
+function handleProtocolUrl(url: string) {
+  logger.debug(`Protocol URL received: ${url}`)
+  // Example: dofustouch://authorized/?code=xxxxx
+  // Extract the authorization code from URL
+  const authCode = url.match(/code=([^&]+)/)
+  if (authCode && authCode[1]) {
+    // Pass the authorization code to game windows
+    if (Application.instance) {
+      Application.instance.handleAuthCode(authCode[1])
+    }
+  }
+}
+
+// Handle the protocol activation for macOS
+app.on('open-url', (event, url) => {
+  event.preventDefault()
+  handleProtocolUrl(url)
+})
+
+// Handle second-instance which can contain the protocol url from user clicks
+app.on('second-instance', (event, commandLine) => {
+  logger.debug('App -> second-instance')
+  
+  // Handle protocol urls on Windows/Linux which are part of the commandLine
+  const protocolUrl = commandLine.find(arg => arg.startsWith('dofustouch://'))
+  if (protocolUrl) {
+    handleProtocolUrl(protocolUrl)
+  }
+  
+  if (Application.instance) {
+    // Focus on the main window if the user tried to open another
+    Application.instance.focusMainWindow()
+  }
+})
 
 app.whenReady().then(async () => {
   logger.debug('App -> whenReady')
@@ -33,6 +78,12 @@ app.whenReady().then(async () => {
   const store = await setupRootStore()
   await Application.init(store)
   Application.instance.run()
+  
+  // Handle protocol urls on Windows/Linux which might be part of process.argv
+  const protocolUrl = process.argv.find(arg => arg.startsWith('dofustouch://'))
+  if (protocolUrl) {
+    handleProtocolUrl(protocolUrl)
+  }
 })
 
 app.on('window-all-closed', () => {
